0d9f212f4c4456b315862d0837eed69c63d883df
M1chalS <michalszajner@wp.pl>
2023-01-02 19:17:49 +0100

    Added custom Table component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import DropdownPage from "./pages/DropdownPage";
 import Sidebar from "./components/Sidebar";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
+import TablePage from "./pages/TablePage";
 
 function App() {
 
     return (<div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
         <Sidebar />
         <div className="col-span-5">
+            <Route path="/table">
+                <TablePage />
+            </Route>
             <Route path="/buttons">
                 <ButtonPage />
             </Route>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ function Sidebar() {
         { label: 'Accordion', path: '/accordion'},
         { label: 'Buttons', path: '/buttons'},
         { label: 'Modal', path: '/modal'},
+        { label: 'Table', path: '/table'},
     ];
 
     const renderedLinks = links.map(link => {
